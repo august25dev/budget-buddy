23ce84a68ce835c51c925331ead2a1257c817ff0
august25dev <august25.dev@gmail.com>
2025-03-07 10:14:49 +0100

    Create app.js
    created app.js and overview

---
+++ app.js
@@ -0,0 +1,24 @@
+// OVERVIEW
+  // INITIAL DATA
+    // - categories array
+  // MODALS
+    // - modal for displaying expenses
+    // - modal2 for expense input
+    // - modal for settings
+  // FUNCTIONS
+    // - loadBudgetTable
+    // - saveExpense
+    // - getExpenses
+    // - displayExpenses
+    // - deleteExpenseFromLocalStorage
+    // - updateAmounts
+    // - openModal
+    // - handleCategoryClick
+    // - enableDynamicDecimalInput
+    // - settings
+    // - updateBudget
+    // - getBudgets
+    // - updateCategories
+    // - initializeBudgets
+    // - updateCategoryNames
+  // MAIN LOGIC
